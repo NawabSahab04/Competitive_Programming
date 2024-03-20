50bd707ec27e4a94aa0329b8fe032acc7b7848c7
Samrath Singh Khanuja <63147553+NawabSahab04@users.noreply.github.com>
2024-03-20 14:15:35 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -5,7 +5,8 @@ using namespace std;
 #define endl "\n" 
 #define int long long
 #define GETBIT(a, n) ((a >> n) & 1)
-#define BITCOUNT(a) __builtin_popcountll(a)
+#define BITCOUNT(a) __builtin_popcount(a)
+//add ll at the end of bitwise functions for long long, else they will convert to int by default
 //BITCOUNT performs an operation for each bit in the given number, and stores the sum
 
 const int MAXN = 200100;
