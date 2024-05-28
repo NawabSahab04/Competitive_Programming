e7d0d712efd6ff7d39602dc5b0049e60e7e890ea
Samrath Singh Khanuja <63147553+NawabSahab04@users.noreply.github.com>
2024-05-28 14:37:36 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -7,7 +7,6 @@ using namespace std;
 #define GETBIT(a, n) ((a >> n) & 1)
 #define BITCOUNT(a) __builtin_popcount(a)
 //add ll at the end of bitwise functions for long long, else they will convert to int by default
-//BITCOUNT performs an operation for each bit in the given number, and stores the sum
 
 const int MAXN = 200100;
 const int MOD = 1000000007;
@@ -33,20 +32,6 @@ int powll(int b, int p)
     forn(i,0,p) a= a*b%MOD;
     return a;
 }
-bool subsequence(string t, string s) {
-    if(s == t)
-    return true;
-    int n = s.size();
-    int m = t.size();
-    int j = 0;
-    for(int i = 0; i < n; i++){
-        if(t[j] == s[i])
-        j++;
-        if(j == t.size())
-        return true;
-    }
-    return false;
-}
 using namespace std;
 void solve() {
     std::cout << std::fixed << std::showpoint << std::setprecision(8);
@@ -57,7 +42,7 @@ int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
     int T = 1;
-    //cin >> T;//Comment this out in case number of test cases are not to be taken as input.
+    //cin >> T;
     for(int I = 1; I <= T; I++) {
         solve(); 
     }
