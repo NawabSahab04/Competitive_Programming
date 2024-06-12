b7ae15380a5b2d9d70e9ba87ef072d2ecc7da4bc
Samrath Singh Khanuja <63147553+NawabSahab04@users.noreply.github.com>
2024-06-12 23:51:42 +0530

    Update combinatorics.cpp

--- combinatorics.cpp
+++ combinatorics.cpp
@@ -1,7 +1,39 @@
-int C(int n, int k) {
-    double res = 1;
-    for (int i = 1; i <= k; ++i)
-        res = res * (n - k + i) / i;
-    return (int)(res + 0.01);
+int binpow(int a, int b, int m) {
+    a %= m;
+    int res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
 }
-
+int modInverse(int n, int p)
+{
+    return binpow(n, p - 2, p);
+}
+ 
+// Returns nCr % p using Fermat's little
+// theorem.
+int C(int n,int r, int p)
+{
+    // If n<r, then nCr should return 0
+    if (n < r)
+        return 0;
+    // Base case
+    if (r == 0)
+        return 1;
+ 
+    // Fill factorial array so that we
+    // can find all factorial of r, n
+    // and n-r
+    int fac[n + 1];
+    fac[0] = 1;
+    for (int i = 1; i <= n; i++)
+        fac[i] = (fac[i - 1] * i) % p;
+ 
+    return (fac[n] * modInverse(fac[r], p) % p
+            * modInverse(fac[n - r], p) % p)
+           % p;
+}
