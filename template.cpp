9ca177db172639ab1cf90c0ff67771689f7bf5cf
Samrath Singh Khanuja <63147553+NawabSahab04@users.noreply.github.com>
2025-05-28 19:25:32 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,4 +1,9 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 using namespace std;
 
 #define forn(i,a,n) for(long long i = a; i < n; i++)
